f02834ae13dc3ee6fc797c65565031ef0539041a
Khiem Pham <76819117+kpham841@users.noreply.github.com>
2021-02-20 11:33:01 -0800

    styled a bit

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,9 +7,13 @@ const Home = () => {
   return (
     <div className="home">
       <h1 className="home-title">
-        A free peer-to-peer network offering resume advice that respects your
+        A free peer-to-peer network offering resume advice
+        </h1>
+        <br/>
+        <h1 className="home-title"> that respects your
         privacy.
       </h1>
+      <br/>
       <h2 className="home-subtitle">
         Get honest feedback and tips to make your resume stand out from people
         who’ve been in your shoes
