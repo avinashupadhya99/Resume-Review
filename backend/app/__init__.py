e4c74302603c106f42984197e05bc794dd525da6
unknown <avinashupadhya99@gmail.com>
2021-02-21 15:17:26 +0530

    Enabled cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_swagger_ui import get_swaggerui_blueprint
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.yml'
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
