91b09e38af77193c9eb9b6b95a1231c945a0daac
Faris Ashai <farisashai@gmail.com>
2021-02-20 18:04:03 -0800

    idk

--- frontend/src/pages/View.js
+++ frontend/src/pages/View.js
@@ -1,7 +1,13 @@
 import React from "react";
 import sampleresume from "../images/sampleresume.jpg";
 
-import { Route, BrowserRouter as Router, Link, Switch } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Link,
+  Switch,
+  useParams,
+} from "react-router-dom";
 
 import {
   Magnifier,
@@ -9,20 +15,26 @@ import {
   SideBySideMagnifier,
   PictureInPictureMagnifier,
   MOUSE_ACTIVATION,
-  TOUCH_ACTIVATION
+  TOUCH_ACTIVATION,
 } from "react-image-magnifiers";
-
-/**
- * view a specific resume with all comments below and timestamp + details to the side maybe
- * probably have to pass in an object representing the resume to the props
- */
+import CommentSection from "../components/CommentSection";
 
 const View = () => {
+  let { id } = useParams();
+  const resume = {
+    /**
+     *TODO
+     */
+  };
+
   return (
     <div className="view">
+      <div className="pad"></div>
       {/** need the database before we can fill */}
       <h1>Resume #1</h1>
+      <h1>{id}</h1>
       <SideBySideMagnifier
+<<<<<<< Updated upstream
       id="pdf" imageSrc={sampleresume} imageAlt="Example" style={{ width: "500px", display: "block", marginLeft: "auto", marginRight:"auto", transform: "translateY(10%)" }} />
       <br/>
       <br/>
@@ -35,13 +47,53 @@ const View = () => {
       <form>
         <textarea id="comment" placeholder="Leave a comment" style={{color:"black", width:"500px", height:"100px", display: "block", marginLeft: "auto", marginRight:"auto", fontSize:"20px", borderRadius: "10px", padding: "10px" }}>
 
+=======
+        id="pdf"
+        imageSrc={sampleresume}
+        imageAlt="Example"
+        style={{
+          width: "500px",
+          display: "block",
+          marginLeft: "auto",
+          marginRight: "auto",
+          transform: "translateY(20%)",
+        }}
+      />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <form>
+        <textarea
+          id="comment"
+          style={{
+            color: "black",
+            width: "500px",
+            height: "100px",
+            display: "block",
+            marginLeft: "auto",
+            marginRight: "auto",
+            fontSize: "20px",
+            borderRadius: "20px",
+            padding: "10px",
+          }}
+        >
+          Leave a comment
+>>>>>>> Stashed changes
         </textarea>
       </form>
 
-      <br/>
-      <br/>
+      <br />
+      <br />
 
       <div className="view-btns">
+<<<<<<< Updated upstream
       <input type="submit" value="Submit" style={{
         color:"white", 
         width:"150px", 
@@ -67,16 +119,49 @@ const View = () => {
         fontSize: "15px",
         cursor: "pointer"
         }}>Cancel</button>
-        </Link>
+=======
+        <input
+          type="submit"
+          value="Submit"
+          style={{
+            color: "black",
+            width: "150px",
+            height: "50px",
+            borderRadius: "20px",
+            border: "none",
+            backgroundColor: "#00d46a",
+            fontSize: "15px",
+            resize: "none",
+          }}
+        />
 
-        </div>
-      
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+        <Link to="/list">
+          <button
+            type="submit"
+            style={{
+              color: "black",
+              width: "150px",
+              height: "50px",
+              borderRadius: "20px",
+              border: "none",
+              backgroundColor: "#d40000",
+              fontSize: "15px",
+            }}
+          >
+            Cancel
+          </button>
+>>>>>>> Stashed changes
+        </Link>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      {/* <CommentSection comments={resume.comments} /> */}
     </div>
   );
 };
