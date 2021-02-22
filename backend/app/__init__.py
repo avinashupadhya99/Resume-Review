e302737b5865e3b8aa222d9aa55df3993c56ce12
unknown <avinashupadhya99@gmail.com>
2021-02-22 12:23:44 +0530

    Added APIs to swagger

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -12,7 +12,7 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+        'app_name': "Resume Review Backend"
     }
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
